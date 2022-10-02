25cb8f30e9e60e10feefa487ac643121247799c0
JDJG Inc. Official <ghostzap123@gmail.com>
2022-10-02 17:22:29 -0400

    Basic Scanner Stuff

--- Testing.java
+++ Testing.java
@@ -1,9 +1,20 @@
-
+import java.util.Scanner;
 
 public class Testing {
 
     public static void main(String[] args) {
 
+        int number1 = 0;
+
         System.out.println("Hello World!");
+
+        Scanner input1 = new Scanner(System.in);
+
+        System.out.println("Please Enter a favorite number");
+        number1 = input1.nextInt();
+
+        System.out.println("You entered: " + number1);
+
+        input1.close();
     }
 }
