2eefa34675526e2f80960880dc28ff2b7984d2e2
JDJG Inc. Official <ghostzap123@gmail.com>
2022-10-05 19:11:55 -0400

    Adds for loop example.

--- Testing.java
+++ Testing.java
@@ -21,8 +21,17 @@ public class Testing {
 
         JOptionPane.showMessageDialog(null, "Hello User");
 
+        //for loop time
 
+        for (int i = 1; i <= 10; i++) {
+
+           System.out.println("number: " + i);
+           //prints number 10 times.
+           //starts at 1 cause it looks better and doing (i + 1) does not look as good.
+
+        
+        } // end for loop
 
         input1.close();
-    }
-}
+    } // end main program
+} //end of actual class
