399de8fbf6185cff2081c07b8682f57afcdcdf8f
JDJG Inc. Official <ghostzap123@gmail.com>
2022-10-21 18:28:28 -0400

    Alright

---
+++ Loops.java
@@ -0,0 +1,23 @@
+public class Loops {
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //for loop
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("number: " + i);
+        }
+        //while loop
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("number: " + i);
+            i++;
+        }
+        //do while loop
+        int j = 1;
+        do {
+            System.out.println("number: " + j);
+            j++;
+        } while (j <= 10);
+    }
+
+}
