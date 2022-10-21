a64c6f2c14b9f7b82264f1641551912c44c860d3
JDJG Inc. Official <ghostzap123@gmail.com>
2022-10-21 18:29:47 -0400

    Github co-pilot generated.

---
+++ LoopArray.java
@@ -0,0 +1,15 @@
+public class LoopArray {
+    
+    public static void main(String[] args) {
+        //loop through array
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+    
+
+}
